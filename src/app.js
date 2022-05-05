26654a6f60f9d0583fe5a8e8eeb30341d2f727fe
EL225 <ericleonmora99@gmail.com>
2022-05-05 02:09:16 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   let iconos = ["♦", "♥", "♠", "♣"];
   let simbolo = ["J", "Q", "R", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  function randumS() {
+    let valor = Math.floor(Math.random() * 13);
+    return valor;
+  }
+  function randumI() {
+    let valor2 = Math.floor(Math.random() * 4);
+    return valor2;
+  }
+  function randomC() {
+    const icono = iconos[randumI()];
+    const numero = simbolo[randumS()];
+    let carta = [icono, numero];
+    return carta;
+  }
+  let card = randomC();
+  if (card[0] == iconos[2] || card[0] == iconos[3]) {
+    document.getElementById("icon1").style.color = "black";
+    document.getElementById("icon2").style.color = "black";
+  }
+
+  document.getElementById("icon1").append(card[0]);
+  document.getElementById("icon2").append(card[0]);
+  document.getElementById("numero").append(card[1]);
 };
